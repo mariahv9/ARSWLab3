d744325ff74d1905ef70be603720e85fe46a9176
mariahv9 <46968626+mariahv9@users.noreply.github.com>
2020-08-26 14:11:01 -0500

    Add files via upload

---
+++ GRAMMAR-CHECKER/src/main/java/edu/eci/arsw/springdemo/EnglishSpellChecker.java
@@ -0,0 +1,14 @@
+package edu.eci.arsw.springdemo;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+@Qualifier ("english")
+public class EnglishSpellChecker implements SpellChecker {
+
+	@Override
+	public String checkSpell(String text) {		
+		return "Checked with english checker:"+text;
+	}
+}
